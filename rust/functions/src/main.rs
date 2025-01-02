f9c7a70a76ff1a61d4114b3f8ccd355b2c7771af
Seol SO <voidcircle080111@gmail.com>
2025-01-02 19:06:58 +0900

    DAY 2: Functions

---
+++ rust/functions/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    println!("Hello, world!");
+    let result: i8 = another_function();
+    print_number(result);
+    print_number(another_function());
+
+    greet("Rust", "beginner", 16);
+
+    let y = {
+        let mut x = 3;
+        x = x + 4;
+        x + 1
+    };
+
+    println!("The y is {y}");
+
+    let mut number_five: u8 = get_five();
+    println!("{number_five}");
+    number_five = plus_one(number_five);
+    println!("{number_five}");
+}
+
+fn another_function() -> i8 {
+    println!("another function!");
+
+    return -2;
+}
+
+fn print_number(number: i8) {
+    println!("The value is {number}");
+}
+
+fn greet(field: &str, job: &str, age: u8) {
+    println!("Hi, I am a {field} {job} and I am {age} years old.");
+}
+
+fn get_five() -> u8 {
+    5
+}
+
+fn plus_one(x: u8) -> u8 {
+    return x + 1;
+}
