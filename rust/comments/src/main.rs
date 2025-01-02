3e25e524d35ecb87c3d34888820cefc1299efc53
Seol SO <voidcircle080111@gmail.com>
2025-01-02 19:07:08 +0900

    DAY 2: Comments

---
+++ rust/comments/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // I am feeling lucky..?
+    let lucky_number: u8 = 7; // you are able to comment like this but it is more like... conventional to
+                              // comment above
+    println!("Are you feeling lucky? If you are not, get a lucky number {lucky_number}!");
+}
