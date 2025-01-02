2ef40da5a25ed77a672f7b879730462e0a0c7031
Seol SO <voidcircle080111@gmail.com>
2025-01-02 22:11:13 +0900

    DAY 2: Branches

---
+++ rust/branches/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn main() {
+    let number: u8 = 12;
+
+    if number < 5 {
+        println!("the condition was true.");
+    } else {
+        println!("the condition was false.");
+    }
+
+    if number != 0 {
+        println!("the number is not zero.");
+    } // the block of code right after the if statement is a expression, which you can also write
+      // like below since expressions are block of codes that do have something to return.
+
+    let is_number_big_enough: bool = if number >= 10 {
+        let x = true;
+        x
+    } else {
+        let x = false;
+        x
+    };
+
+    // but the below is more... better.
+
+    let condition = number > 10;
+    let is_number_big_enough: u8 = if condition { 5 } else { 200 }; // of course, each `ARM` should
+                                                                    // return the same type of data
+
+    println!("{is_number_big_enough}");
+}
