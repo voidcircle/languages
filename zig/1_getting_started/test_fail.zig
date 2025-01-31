4e10d9947c0037bda09e5d8b18f127f0e83c0b34
Seol SO <voidcircle080111@gmail.com>
2025-01-31 18:35:17 +0900

    remove zig

--- zig/1_getting_started/test_fail.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-const expect = std.testing.expect;
-
-test "always failed?" {
-    try expect(false);
-}
--- zig/README.md
+++
@@ -1,4 +0,0 @@
-# Zig
-
-- [Zig.guide](https://zig.guide/)
-- [Zig Official Doc](https://ziglang.org/learn/)
